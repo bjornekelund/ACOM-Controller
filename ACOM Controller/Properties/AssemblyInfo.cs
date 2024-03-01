7c7724ab74766a123fc5c81e18ce02857734d11d
Björn Ekelund <bjorn@ekelund.nu>
2024-03-01 17:56:19 +0100

    Release 2.4.8.0

--- ACOM Controller/Properties/AssemblyInfo.cs
+++ ACOM Controller/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SM7IUN")]
 [assembly: AssemblyProduct("ACOM Controller")]
-[assembly: AssemblyCopyright("© Björn Ekelund SM7IUN 2023")]
+[assembly: AssemblyCopyright("© Björn Ekelund SM7IUN 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.4.7.0")]
-[assembly: AssemblyFileVersion("2.4.7.0")]
+[assembly: AssemblyVersion("2.4.8.0")]
+[assembly: AssemblyFileVersion("2.4.8.0")]
